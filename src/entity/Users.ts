61256e9e567dd3532325201bb7e743e490bcd628
nedu <nedufari@gmail.com>
2022-11-15 14:48:24 +0100

    added new one

---
+++ src/entity/Users.ts
@@ -0,0 +1,14 @@
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+
+@Entity()
+export class User extends BaseEntity{
+    @PrimaryGeneratedColumn()
+    id:number
+
+    @Column({unique:true, nullable:false})
+    email:string
+
+    @Column()
+    password:string
+}
